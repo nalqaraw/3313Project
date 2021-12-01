6bbf8062f0a7de0d483358fc663b5d1b5e1370fb
nalqaraw <nalqaraw@uwo.ca>
2021-11-30 23:10:01 -0500

    update5

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const io = new Server(server);
 
 chatApp.use(express.static("clients"));
 
-// Global variables to hold all guest names and rooms created
+// Global variables to hold all guest names and chatRooms created
 var guests = {};
-var rooms = [
-  { name: "main", creator: "Anonymous" },
-  { name: "Room 2", creator: "Anonymous" },
+var chatRooms = [
+  { name: "main", creator: " " },
+  { name: "Room 2", creator: " " },
+  { name: "Room 3", creator: " " },
 ];
 
-io.on("connection", socket => {
-  console.log(`User connected to server.`);
+io.on("joining", socket => {
 
   socket.on("createUser", username =>{
     socket.username = username;
@@ -24,24 +24,22 @@ io.on("connection", socket => {
     socket.currentRoom = "main";
     socket.join("main");
 
-    console.log(`User ${username} created on server successfully.`);
-
     socket.emit("updateChat", "INFO", "You have joined main room");
     socket.broadcast
       .to("main")
       .emit("updateChat", "INFO", username + " has joined the main room");
     io.sockets.emit("updateUsers", guests);
-    socket.emit("updateRooms", rooms, "main");
+    socket.emit("updateRooms", chatRooms, "main");
   });
 
   socket.on("sendMessage", data => {
     io.sockets.to(socket.currentRoom).emit("updateChat", socket.username, data);
   });
 
-  socket.on("createRoom", room=> {
-    if (room != null) {
-      rooms.push({ name: room, creator: socket.username});
-      io.sockets.emit("updateRooms", rooms, null);
+  socket.on("createRoom", chatRooms=> {
+    if (chatRooms != null) {
+        chatRooms.push({ name: chatRooms, creator: socket.username});
+      io.sockets.emit("updateRooms", chatRooms, null);
     }
   });
 
@@ -63,7 +61,6 @@ io.on("connection", socket => {
   });
 
   socket.on("disconnect",  ()=> {
-    console.log(`User ${socket.username} has disconnected.`);
     delete guests[socket.username];
     io.sockets.emit("updateUsers", guests);
     socket.broadcast.emit(
